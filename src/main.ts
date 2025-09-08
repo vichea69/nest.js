ff62676393d6f6d4ebebc5dde2742e9482b5319b
Vichea69 <smart.vichea@gmail.com>
2025-09-08 14:11:52 +0700

    last update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from '@nestjs/common';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import {ResponseInterceptor} from './common/interceptors/response.interceptor';
+import {AllExceptionsFilter} from './common/filters/http-exception.filter';
 import cookieParser from 'cookie-parser';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -15,6 +17,29 @@ async function bootstrap() {
     });
     app.use(cookieParser());
     app.setGlobalPrefix('api/v1')
+
+    // Swagger/OpenAPI setup
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('API Documentation')
+        .setDescription('REST API documentation')
+        .setVersion('1.0')
+        .addBearerAuth(
+            {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+                in: 'header',
+            },
+            'bearer',
+        )
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig, {
+        ignoreGlobalPrefix: false,
+    });
+    SwaggerModule.setup('api/v1/docs', app, document, {
+        swaggerOptions: {persistAuthorization: true},
+    });
 
     app.useGlobalPipes(
         new ValidationPipe({
