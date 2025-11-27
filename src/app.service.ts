9859038511ec39f72fe533dda2f7f1a0ed148990
Vichea69 <smart.vichea@gmail.com>
2025-11-27 11:36:33 +0700

    🚀 Add CI/CD pipeline for NestJS deployment via self-hosted runner

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'abbbs';
+        return 'Yo Bro your API is running Now!! 🔥🏃';
     }
 }
