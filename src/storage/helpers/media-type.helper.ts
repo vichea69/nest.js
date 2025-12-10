543309e26241dfdb8a95deb14f7709e949a42589
Vichea69 <smart.vichea@gmail.com>
2025-12-10 14:57:48 +0700

    🪭 Media : Add support file type SVG!

--- src/storage/helpers/media-type.helper.ts
+++ src/storage/helpers/media-type.helper.ts
@@ -2,6 +2,7 @@
 export function detectMediaType(mimeType: string): string {
     if (mimeType.startsWith('image/')) return 'image';
     if (mimeType.startsWith('video/')) return 'video';
+    if (mimeType.startsWith('svg/')) return 'svg';
     if (mimeType === 'application/pdf') return 'pdf';
     if (
         mimeType.startsWith('application/') ||
